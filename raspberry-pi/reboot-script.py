a1ca0e4f78e55f1ab4aca739ac32a8d27ff268b0
Jacob David C Cunningham <greenace92@gmail.com>
2018-08-02 18:41:29 -0500

    Update reboot-script.py

--- raspberry-pi/reboot-script.py
+++ raspberry-pi/reboot-script.py
@@ -2,6 +2,7 @@ import os
 
 # CRON will call this script every 5 minutes to ping a remote ip
 # restart if it fails
+# the thread plot starts on reboot with a PID to prevent redundant calls
 
 # reconnect to WiFi by restarting
 hostname = 'www.example.com' # or other domain
